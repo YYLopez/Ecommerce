5885b0063a89bde6eb283fe820d4e69b798ffca5
Yoel Lopez <yoellopez@Yoels-MacBook-Pro.local>
2018-02-25 19:30:35 -0300

    Boton Ad to Shopping Cart.

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var users = require('./routes/users');
 
 var app = express();
 
-mongoose.connect('localhost:27017/shopping');
+mongoose.connect('mongodb://localhost:27017/shopping');
 
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -1,40 +1,40 @@
 var Product = require('../models/product');
 var mongoose = require('mongoose');
 
-mongoose.connect('localhost:27017/shopping');
+mongoose.connect('mongodb://localhost:27017/shopping');
 
 var products = [
 
     new Product({
-        imagePath: '../public/images/productos/producto1-1.jpg',
+        imagePath: '/public/images/productos/producto1-1.jpg',
         title: 'T- Shirt Mistral',
         description: 'Awesome Product',
         price: 10
     }),
 
     new Product({
-        imagePath: '../public/images/productos/producto2-1.jpg',
+        imagePath: 'public/images/productos/producto2-1.jpg',
         title: 'T- Shirt Mistral',
         description: 'Awesome Product',
         price: 20
     }),
 
     new Product({
-        imagePath: '../public/images/productos/producto3-1.jpg',
+        imagePath: 'public/images/productos/producto3-1.jpg',
         title: 'T- Shirt Mistral',
         description: 'Awesome Product',
         price: 40
     }),
 
     new Product({
-        imagePath: '../public/images/productos/producto2-1.jpg',
+        imagePath: 'public/images/productos/producto2-1.jpg',
         title: 'T- Shirt Mistral',
         description: 'Awesome Product',
         price: 50
     }),
 
     new Product({
-        imagePath: '../public/images/productos/producto1-1.jpg',
+        imagePath: 'public/images/productos/producto1-1.jpg',
         title: 'T- Shirt Mistral',
         description: 'Wonderful Product',
         price: 100
