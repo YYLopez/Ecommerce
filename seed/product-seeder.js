b1d9f19e0c4090f04025a365774ef6acc0be639e
Yoel Lopez <yoellopez@Yoels-MacBook-Pro.local>
2018-02-25 16:25:48 -0300

    - Agregado de Imagenes de Productos - Carga de seeder productos en la base de datos.

---
+++ seed/product-seeder.js
@@ -0,0 +1,41 @@
+var Product = require('../models/product');
+
+
+var products = [
+
+    new Product({
+        imagePath: '../public/images/productos/producto1-1.jpg',
+        title: 'T- Shirt Mistral',
+        description: 'Awesome Product',
+        price: 10
+    }),
+
+    new Product({
+        imagePath: '../public/images/productos/producto2-1.jpg',
+        title: 'T- Shirt Mistral',
+        description: 'Awesome Product',
+        price: 20
+    }),
+
+    new Product({
+        imagePath: '../public/images/productos/producto3-1.jpg',
+        title: 'T- Shirt Mistral',
+        description: 'Awesome Product',
+        price: 40
+    }),
+
+    new Product({
+        imagePath: '../public/images/productos/producto2-1.jpg',
+        title: 'T- Shirt Mistral',
+        description: 'Awesome Product',
+        price: 50
+    }),
+
+    new Product({
+        imagePath: '../public/images/productos/producto1-1.jpg',
+        title: 'T- Shirt Mistral',
+        description: 'Wonderful Product',
+        price: 100
+    })
+
+];
